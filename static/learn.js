5f8de61ebbc56002ca59fe94d0ea1408fe2c2269
ethmth <ethan@smaze.net>
2024-04-16 14:25:37 -0400

    Made text appear on new line

--- static/learn.js
+++ static/learn.js
@@ -1,35 +1,3 @@
-// const divViewRating = "#view-entry-rating";
-// const divViewDifficulty = "#view-entry-difficulty";
-// const divViewArches = "#view-entry-arches";
-
-// const divViewOthersFamily = "#view-entry-other-distros-family";
-// const divViewOthersDifficulty = "#view-entry-other-distros-difficulty";
-
-// function set_rating(entry) {
-//   let div = get_rating_div(entry.expert_rating);
-//   $(divViewRating).append(div);
-// }
-
-// function set_difficulty(entry) {
-//   let div = get_experience_div(entry.experience_level, true);
-//   $(divViewDifficulty).append(div);
-// }
-
-// function set_arches(entry) {
-//   let div = get_architectures_div(entry.architectures, "", true);
-//   $(divViewArches).append(div);
-// }
-
-// function set_others_family(others_family) {
-//   let div = get_similar_links(select_n_entries(others_family, 3, true));
-//   $(divViewOthersFamily).append(div);
-// }
-
-// function set_others_difficulty(others_difficulty) {
-//   let div = get_similar_links(select_n_entries(others_difficulty, 3, true));
-//   $(divViewOthersDifficulty).append(div);
-// }
-
 const VIDEO_DOMAIN = "https://droplet.ethanmt.com/videos/pitching/";
 const slideDiv = "#slide";
 let slides = {};
@@ -65,16 +33,20 @@ function getSlideMediaDiv(slideMedia) {
 function renderSlide(slide) {
   $(slideDiv).empty();
 
+  const slideText = slide["slideText"].replace("\n", "<br/>");
+
   let slide_div = $("<div>").addClass("");
   let slide_title = $("<h1>").addClass("");
-  let slide_text = $("<h2>").addClass("");
+  let slide_text = $("<h2>").addClass("text-left");
   let slide_media_div = $("<div>");
   if(slide["slideMedia"]) {
     slide_media_div = getSlideMediaDiv(slide["slideMedia"]);
   }
 
   slide_title.text(slide["slideName"]);
-  slide_text.text(slide["slideText"]);
+  slide_text.html(slideText);
+
+  console.log(slide["slideText"]);
 
   slide_div.append(slide_title, slide_text, slide_media_div);
 
@@ -84,19 +56,15 @@ function renderSlide(slide) {
 function renderCurrentSlide() {
   const slide = slides[currentSlide];
 
-  // console.log(slide);
   renderSlide(slide);
 }
 
 function initializeSlides(slides_array) {
-  // console.log(slides_array);
-
   const slides_dict = slides_array.reduce((acc, currentValue, index) => {
     acc[index] = currentValue;
     return acc;
   }, {});
 
-  // console.log(slides_dict);
   slides = slides_dict;
 }
 
